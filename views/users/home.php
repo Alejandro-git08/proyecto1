627dff5823c97f152be81c0066d2957176848cdb
Alejandro <alejandro.santos2.2.6.o@gmail.com>
2025-11-26 20:12:36 -0500

    url de los js arreglados

--- views/users/home.php
+++ views/users/home.php
@@ -219,11 +219,11 @@
     </footer>
     <!-- End Footer -->
 
-    <script src="/proyectos/proyecto1/assets/js/jquery-1.11.0.min.js"></script>
-    <script src="/proyectos/proyecto1/assets/js/jquery-migrate-1.2.1.min.js"></script>
-    <script src="/proyectos/proyecto1/assets/js/bootstrap.bundle.min.js"></script>
-    <script src="/proyectos/proyecto1/assets/js/templatemo.js"></script>
-    <script src="/proyectos/proyecto1/assets/js/custom.js"></script>
+    <script src="assets/js/jquery-1.11.0.min.js"></script>
+    <script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/templatemo.js"></script>
+    <script src="assets/js/custom.js"></script>
 
 </body>
 </html>
